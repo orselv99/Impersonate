a3514278e54540e30bf67c33b851524f0ed0bfba
orseLv99 <orselv99@gmail.com>
2023-03-25 21:20:02 +0900

    request 한건씩만 처리하게 pipe 서버구조 변경

--- Impersonate/Util/PipeServer.cs
+++ Impersonate/Util/PipeServer.cs
@@ -7,7 +7,9 @@ namespace Impersonate.Util
 {
     public class PipeServerUtil
     {
+        private string pipe_name = "";
         private NamedPipeServerStream server = null;
+        private PipeSecurity pipe_security = null;
         private Thread worker = null;
         private bool is_terminated = false;
         public delegate Process external_method(string application_name, string command_line);
@@ -18,13 +20,18 @@ namespace Impersonate.Util
             // impersonate 실행 메서드
             this.method = method;
 
-            // 다른 권한의 프로세스로와 연결을 위한 설정
-            var psRule = new PipeAccessRule(@"Everyone", PipeAccessRights.ReadWrite, System.Security.AccessControl.AccessControlType.Allow);
-            var ps = new PipeSecurity();
-            ps.AddAccessRule(psRule);
+            // pipe 서버이름
+            this.pipe_name = pipe_name;
 
-            // 하나의 접속만 허용
-            this.server = new NamedPipeServerStream(pipe_name, PipeDirection.In, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous, 1, 1, ps);
+            // 다른 권한의 프로세스로와 연결을 위한 설정
+            var pipe_access_rule = new PipeAccessRule(
+                "Everyone", 
+                PipeAccessRights.ReadWrite, 
+                System.Security.AccessControl.AccessControlType.Allow
+            );
+            this.pipe_security = new PipeSecurity();
+            this.pipe_security.AddAccessRule(pipe_access_rule);
+
             this.worker = new Thread(this.ThreadWorker);
             this.worker.Start();
         }
@@ -35,24 +42,43 @@ namespace Impersonate.Util
             {
                 try
                 {
+                    // 하나의 접속만 허용
+                    this.server = new NamedPipeServerStream(
+                        this.pipe_name, 
+                        PipeDirection.In, 
+                        1, 
+                        PipeTransmissionMode.Byte, 
+                        PipeOptions.Asynchronous, 
+                        1, 
+                        1, 
+                        this.pipe_security
+                    );
+                
+                    // 연결대기
                     this.server.WaitForConnection();
 
                     while (this.server.IsConnected == true)
                     {
                         byte[] read = new byte[byte.MaxValue];
-                        int result = server.Read(read, 0, read.Length);
-                        if (result > 0)
+                        if (server.Read(read, 0, read.Length) > 0)
                         {
                             /* TODO: 프로토콜을 만들어 처리 */
 
                             // 실행할 process 와 parameter 전달
                             var process = this.method("{PROCESS}", "{PARAMETERS}");
                         }
+
+                        break;
                     }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
+                }
+                finally
+                {
+                    this.server.Close();
+                    this.server = null;
                 }
             }
         }
